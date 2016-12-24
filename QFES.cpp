a62a1c598fc36c1d2e60081443b8d76f445d3f3a
Alexander Kraus <alexander.kraus@student.kit.edu>
2016-12-24 11:39:20 +0100

    changed .gitignore to exclude kdevelop files and build directory.\nAdded license to QFES.cpp.\nFixed definitions in CMakeLists.txt.

--- QFES.cpp
+++ QFES.cpp
@@ -1,7 +1,29 @@
+/*    
+ *  QFES - QT Foil+Epee+Saber: Fencing tournament software. 
+ *  Copyright (C) 2016  Alexander Kraus <alexander.kraus@student.kit.edu>
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdio.h>
 #include <stdlib.h>
 
 int main(int argc, char **args)
 {
+    printf("QFES  Copyright (C) 2016  Alexander Kraus\n\
+        This program comes with ABSOLUTELY NO WARRANTY; for details see `Help->About'.\n\
+        This is free software, and you are welcome to redistribute it\n\
+        under certain conditions; see `Help->About' for details.\n");
     return 0;
 }
